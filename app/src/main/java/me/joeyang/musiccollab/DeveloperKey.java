79160fc8e33725da091e98dc73475099ea8854e4
joeyang22 <jy22yang@uwaterloo.ca>
2016-01-05 21:19:32 -0500

    Added Youtube Player to RoomFragment

---
+++ app/src/main/java/me/joeyang/musiccollab/DeveloperKey.java
@@ -0,0 +1,8 @@
+package me.joeyang.musiccollab;
+
+/**
+ * Created by Joe Yang on 1/4/2016.
+ */
+public class DeveloperKey {
+    public static final String DEVELOPER_KEY = "AIzaSyCnblhHPzMw87W_E_ATgeKObfZxS_m5ehw";
+}
--- app/src/main/java/me/joeyang/musiccollab/Fragments/RoomFragment.java
+++ app/src/main/java/me/joeyang/musiccollab/Fragments/RoomFragment.java
@@ -1,17 +1,27 @@
 package me.joeyang.musiccollab.Fragments;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+
+import me.joeyang.musiccollab.DeveloperKey;
 import me.joeyang.musiccollab.R;
 
-public class RoomFragment extends android.support.v4.app.Fragment {
+public class RoomFragment extends Fragment {
     private static final String KEY_ROOM_ID = "room_id";
+    private static final String LOG_TAG = RoomFragment.class.getSimpleName();
     TextView mRoomText;
     String roomId;
+    YouTubePlayerSupportFragment mYouTubePlayerFragment;
+
     public static RoomFragment newInstance(String roomId) {
         Bundle bundle = new Bundle();
         bundle.putString(KEY_ROOM_ID, roomId);
@@ -37,7 +47,27 @@ public class RoomFragment extends android.support.v4.app.Fragment {
         View rootView = inflater.inflate(R.layout.fragment_room, container, false);
         mRoomText = (TextView) rootView.findViewById(R.id.roomNameTextView);
         mRoomText.setText(String.format(getString(R.string.room_name), roomId));
+
+        YouTubePlayerSupportFragment youTubePlayerFragment = YouTubePlayerSupportFragment.newInstance();
+
+        youTubePlayerFragment.initialize(DeveloperKey.DEVELOPER_KEY, new YouTubePlayer.OnInitializedListener() {
+
+
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.cueVideo("2zNSgSzhBfM");
+                youTubePlayer.play();
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        });
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.youtubeSupportFragment, youTubePlayerFragment).commit();
         return rootView;
     }
+
 
 }
